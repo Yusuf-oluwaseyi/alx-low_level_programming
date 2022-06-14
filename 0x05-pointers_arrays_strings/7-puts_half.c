ca5de81268a14e6b2389991dfe4dff5c5b089a03
Yusuf-oluwaseyi <tobacrown@gmail.com>
2022-06-13 18:19:15 -0700

    Adding puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 	int count = 0, i;
 
-	while (count > = 0)
+	while (count >= 0)
 	{
 		if (str[count] == '\0')
 			break;
@@ -21,7 +21,7 @@ void puts_half(char *str)
 	else
 		i = (count - 1) / 2;
 
-	for (i++; i < count, i++)
+	for (i++; i < count; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
