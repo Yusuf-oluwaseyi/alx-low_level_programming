23d9d1f54e742db50351f56529bbca4584bcd91e
Yusuf-oluwaseyi <tobacrown@gmail.com>
2022-06-13 19:11:33 -0700

    keygeneration

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 		if (*(s + count) == '-')
 			pn *= -1;
 
-		if ((*(s + count ) >= '0') && (*(s + count) <= '9'))
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
