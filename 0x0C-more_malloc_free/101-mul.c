742c28f0843cb0dceaf3a5f0a033faf319a367fe
Yusuf-oluwaseyi <tobacrown@gmail.com>
2022-06-30 17:59:11 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,7 +16,8 @@ void _is_zero(char *argv[])
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
-			isn1 = 0;																break;
+			isn1 = 0;
+			break;
 		}
 
 	for (i = 0; argv[2][i]; i++)
