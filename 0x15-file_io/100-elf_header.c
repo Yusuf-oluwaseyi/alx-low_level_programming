ac798e3d6ae704461c36c3ef15c20ff5bc27e4a2
Yusuf-oluwaseyi <tobacrown@gmail.com>
2022-07-25 14:21:31 -0700

    Task 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -175,72 +175,72 @@ void check_sys(char *ptr)
 		printf("  Class:                             ELF64\n");
 
 	print_data(ptr);
-									print_version(ptr);
-										print_osabi(ptr);
-											print_type(ptr);
-												print_addr(ptr);
+	print_version(ptr);
+	print_osabi(ptr);
+	print_type(ptr);
+	print_addr(ptr);
 }
 
 /**
- *  * check_elf - check if it is an elf file.
- *   * @ptr: magic.
- *    * Return: 1 if it is an elf file. 0 if not.
- *     */
+ * check_elf - check if it is an elf file.
+ * @ptr: magic.
+ * Return: 1 if it is an elf file. 0 if not.
+ */
 int check_elf(char *ptr)
 {
-		int addr = (int)ptr[0];
-			char E = ptr[1];
-				char L = ptr[2];
-					char F = ptr[3];
+	int addr = (int)ptr[0];
+	char E = ptr[1];
+	char L = ptr[2];
+	char F = ptr[3];
 
-						if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
-									return (1);
+	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+		return (1);
 
-							return (0);
+	return (0);
 }
 
 /**
- *  * main - check the code for Holberton School students.
- *   * @argc: number of arguments.
- *    * @argv: arguments vector.
- *     * Return: Always 0.
- *      */
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-		int fd, ret_read;
-			char ptr[27];
+	int fd, ret_read;
+	char ptr[27];
 
-				if (argc != 2)
-						{
-									dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-											exit(98);
-												}
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
 
-					fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 
-						if (fd < 0)
-								{
-											dprintf(STDERR_FILENO, "Err: file can not be open\n");
-													exit(98);
-														}
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+		exit(98);
+	}
 
-							lseek(fd, 0, SEEK_SET);
-								ret_read = read(fd, ptr, 27);
+	lseek(fd, 0, SEEK_SET);
+	ret_read = read(fd, ptr, 27);
 
-									if (ret_read == -1)
-											{
-														dprintf(STDERR_FILENO, "Err: The file can not be read\n");
-																exit(98);
-																	}
+	if (ret_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+		exit(98);
+	}
 
-										if (!check_elf(ptr))
-												{
-															dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
-																	exit(98);
-																		}
+	if (!check_elf(ptr))
+	{
+		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+		exit(98);
+	}
 
-											check_sys(ptr);
-												close(fd);
+	check_sys(ptr);
+	close(fd);
 
-													return (0);
+	return (0);
 }
